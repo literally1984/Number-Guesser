291f462030546e72e9adcfcde8ad6fa1a780956b
legitimately_the_1984th_year_of_the_common_era_&_anno_domini_designations <85363779+literally1984@users.noreply.github.com>
2021-06-11 11:56:10 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+print('Please think of a number between 0 and 100!')
+maxim = 100
+incrdcrby = 0.00000000000001
+low = 0
+high = maxim
+while True:
+    guess = (high + low) // 2
+    print('Is your secret number ' + str(guess) + '?')
+    print('')
+    x = str(input("Enter 'h' to indicate the guess is too high. \nEnter 'l' to indicate the guess is too low. \nEnter 'c' to indicate the guess is correct. "))
+    if x == 'h':
+      high = guess
+    elif x == 'l':
+      low = guess
+    elif x == 'c':
+      break
+    else:
+      print('Sorry, I did not understand your input.')
+print('Game over. Your secret number was: ' + str(guess))
